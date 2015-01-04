016c16a83bc0d05770b63f64b3f8774dd5d33b6b
Frank Kalbermatten <frankkalbermatten@gmail.com>
2015-01-04 17:13:43 -0300

    video section

--- js/app.js
+++ js/app.js
@@ -13,79 +13,21 @@ $('.off-canvas-wrap').on('click', function (e) {
     $(this).foundation('offcanvas', 'hide', 'offcanvas-overlap-right');
 });
 
-$('.modal-trigger').on('click', function(e) {
-    var modalElement = $(this).attr('href');
 
-    e.preventDefault();
-    $(modalElement).toggleClass('hide');
-});
+ function toggleModal(element) {
+     element.toggleClass('hide');
+ }
 
+ $('.modal-trigger').on('click', function(e) {
+     var $modalElement = $(this).closest('.slide-panel').next();
 
-$('.multiple-items').slick({
-    infinite: false,
-    arrows: true,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    slide: "li",
-    draggable: false,
-    touchMove: false,
-    touchThreshold: 10,
-    responsive: [
-        {
-            breakpoint: 1025, // Only Landscape Tablet
-            settings: {
-                slidesToShow: 3,
-                arrows:false
-            }
-        },
-        {
-            breakpoint: 769, // Only Portrait Tablet
-            settings: {
-                slidesToShow: 2,
-                arrows:false
-            }
-        },
-        {
-            breakpoint: 480, // Only Portrait Mobile
-            settings: {
-                slidesToShow: 1,
-                arrows:false
-            }
-        },
-        {
-            breakpoint: 640, // Only Landscape Mobile
-            settings: {
-                slidesToShow: 4,
-                arrows:false
-            }
-        }
-    ],
-    onInit: function () {
-        // Tabs Functionality
-        var $slickSlide = $('.slick-slide').children();
-        var $tabContentChildrens = $('.tabs-content').children('.content');
-        var $tabContentId;
-        var groupElement;
+     e.preventDefault();
+     toggleModal($modalElement);
+ });
 
-        function removeActive (element1, element2) {
-            groupElement = element1.add(element2);
-            groupElement.removeClass('active');
-        }
+ $('.close-modal').on('click', function(e) {
+     var $modalElement = $(this).parent();
 
-        function addActive (element1, element2) {
-            groupElement = element1.add(element2);
-            groupElement.addClass('active');
-        }
-
-        $slickSlide.on("click", function(e){
-            e.preventDefault();
-
-            $tabContentId = $(this).attr('href');
-
-            removeActive($slickSlide.parent(), $tabContentChildrens);
-            addActive($(this).parent(), $($tabContentId));
-
-
-        });
-    }
-});
+     e.preventDefault();
+     toggleModal($modalElement);
+ });
