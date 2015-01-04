6840360453e671f10012ea29e4e983d5afdbbfab
Frank Kalbermatten <frankkalbermatten@gmail.com>
2015-01-04 19:28:58 -0300

    video section

---
+++ js/youtube-iframe-video.js
@@ -0,0 +1,50 @@
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+var player;
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+function onYouTubeIframeAPIReady() {
+    var container;
+    var videoId;
+
+    $('.modal-trigger').on('click', function (e) {
+        e.preventDefault();
+        container = $(this).closest('.slide-panel').next().children('.player-video').attr('id');
+        videoId = $(this).closest('.slide-panel').next().children('.player-video').attr('data-video');
+        playerVideo(container, videoId);
+    });
+
+    function playerVideo (container, videoId) {
+        player = new YT.Player(container, {
+            playerVars: {
+              'showinfo': 0,
+              'theme': 'light',
+              'rel': 0
+            },
+            videoId: videoId,
+            events: {
+                'onReady': onPlayerReady
+            }
+        });
+    }
+
+}
+
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+    event.target.playVideo();
+
+    $('.close-modal').on('click', function (event) {
+        event.preventDefault();
+        stopVideo();
+    });
+
+}
+
+function stopVideo() {
+    player.stopVideo();
+
+}
