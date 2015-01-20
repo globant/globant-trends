12011b456f6df88d1688e603a10b13d6724757ae
Frank Kalbermatten <frankkalbermatten@gmail.com>
2015-01-19 21:33:28 -0300

    open offcanvas one at time

--- js/jquery.indexPage.js
+++ js/jquery.indexPage.js
@@ -10,14 +10,12 @@
             $('.off-canvas-wrap', $this).on('click.off-canvas', function (e) {
                 e.preventDefault();
                 var $this = $(this);
-                $this.foundation('offcanvas', 'hide', 'move-left');
-                $this.foundation('offcanvas', 'hide', 'move-right');
+                $this.foundation('offcanvas', 'hide', 'move-left').foundation('offcanvas', 'hide', 'move-right');
             });
 
             $this.on('open.fndtn.offcanvas', '[data-offcanvas]', function () {
                 var $childrenInnerWrap =  $(this).children('.inner-wrap');
-                $this.foundation('offcanvas', 'hide', 'move-left');
-                $this.foundation('offcanvas', 'hide', 'move-right');
+                $this.foundation('offcanvas', 'hide', 'move-left').foundation('offcanvas', 'hide', 'move-right');
                 $childrenInnerWrap.addClass('off-canvas-open');
                 $childrenInnerWrap.children('[data-trigger]').addClass('hide');
             });
